a11d4b60db657d621417b576b9086fe0c5ed9250
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-11-16 16:08:09 -0600

    some fixing to do later <(＿　＿)>

---
+++ MCVProj2/MCVProj2/Controllers/OUController.cs
@@ -0,0 +1,40 @@
+﻿using MCVProj2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MCVProj2.Controllers
+{
+    public class OUController : Controller
+    {
+        // GET: OU
+        public ActionResult Index()
+        {
+            return View();
+
+
+        }
+        public ActionResult TAs()
+        {
+            List<Student> tas = new List<Student>();
+            {
+                new Student()
+                {
+                    FirstName = "Jenn",
+                    LastName = "Ha",
+                    SoonerID = 8
+                };
+                new Student()
+                {
+                    FirstName = "Nhi",
+                    LastName = "Ha",
+                    SoonerID = 9
+                };
+                   
+            };
+            return View; 
+        }
+    }
+}
---
+++ MCVProj2/MCVProj2/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MCVProj2.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        [DisplayName("TA LastName")]
+        public string LastName { get; set; }
+        public int SoonerID { get; set; }
+    }
+}
